0bc84d0d6c40b8704a4b544f4d418a8ca3b0879e
Arthur Tsvettsih <ape08@478-mac-06.sep.cs.ox.ac.uk>
2018-01-15 17:52:49 +0000

    # Modified the hello world string

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,7 +7,7 @@ public class QueryProcessor {
 					"English poet, playwright, and actor, widely regarded as the greatest " +
 					"writer in the English language and the world's pre-eminent dramatist.";
 		} else if (query.toLowerCase().contains("hello world")) {
-			return "Hello World To You Too 8)";
+			return "Hello World To You Too!";
 		}
 		return "";
 	}
